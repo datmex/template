cb3cfe6db3085837af6ec0320ec13f9fd06d022e
edgar <edgar.mtz.andrade@gmail.com>
2019-10-17 13:28:53 -0500

    primer v

---
+++ application.py
@@ -0,0 +1,36 @@
+
+from flask import Flask, jsonify
+import flask_restful
+from pymongo import MongoClient
+from flask import make_response
+from bson.json_util import dumps
+from flask_restful import Api, Resource
+#import resources
+
+
+app = Flask(__name__)
+api = Api(app)
+uri = "mongodb://inegi:rsF7vS5QFZmUCLTHNo7sEYTN3lOxVkHJSH6Vf3xlSGbj8hXzM3hGxZHNxTe9Fny296ya4BgjnSetDZyrj4b8yA==@inegi.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
+db = MongoClient(uri)
+mongo = db.inegi
+
+def output_json(obj, code, headers=None):
+    resp = make_response(dumps(obj), code)
+    resp.headers.extend(headers or {})
+    return resp
+
+class Root(Resource):
+    def get(self):
+        res = mongo.denue.find().limit(1)
+        resp = dumps(res,indent=1, ensure_ascii=False).encode('utf8')
+        return jsonify(resp)
+    
+class Hola(Resource):
+    def get(self):
+        return "Hola mundo!"
+
+api.add_resource(Hola, '/d')
+api.add_resource(Root, '/')        
+
+if __name__ == "__main__":
+	app.run()
